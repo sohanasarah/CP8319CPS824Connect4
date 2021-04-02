5aadbf02431aaf6d4bfc3cb22bb50647132717d2
Sarah Sohana <sarahsohana@gmail.com>
2021-04-02 17:45:55 -0400

    two players playing against each other

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ from game import Game
 from player import  Player
 from game_controller import GameController
 from RNNModel import ConnectFourModelRNN
-
+from model import ConnectFourModel
 
 playerOneVal= -1
 playerTwoVal = 1
 
 def main():
     y = []
-    x=[]
-    i = 14000
-    while i <= 14000:
-        print(i)
+    x= []
+#     i = 14000
+    i= 1
+    while i <= 100:
+        print('value is', i)
         x.append(i)
         firstGame = Game()
         playerOne = Player(playerOneVal, strategy='random')
@@ -22,20 +23,21 @@ def main():
         print ("Playing with both players with random strategies")
         gameController.simulateManyGames(i)
 
-        model = ConnectFourModelRNN(42, 3, 50, 100)
-        model.train(gameController.getTrainingHistory())
+        RNNmodel = ConnectFourModelRNN(42, 3, 50, 100)
+        RNNmodel.train(gameController.getTrainingHistory())
+        
+        DQNmodel = ConnectFourModel(42, 3, 50, 100)
+        DQNmodel.train(gameController.getTrainingHistory())
 
-        playerOneNeural = Player(playerOneVal, strategy='model', model=model)
-#         playerTwoNeural = Player(playerTwoVal, strategy='model', model=model)
+        playerOneNeural = Player(playerOneVal, strategy='model', model=RNNmodel)         
+        playerTwoRNN = Player(playerTwoVal, strategy='model', model=DQNmodel)
 
         secondGame = Game()
-        gameController = GameController(secondGame, playerOneNeural, playerTwo)
-        print("Playing with player 1 as Neural Network")
+        gameController = GameController(secondGame, playerOneNeural, playerTwoRNN)
+        print("Playing with RNN vs DQN")
         res = gameController.simulateManyGames(100)
         y.append(res)
-        i+= 500
-
-        break
+        i += 1
 
     return x, y
 
